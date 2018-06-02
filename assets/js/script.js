e6dc5dfe379ec7606f70044756519cc4e7f24376
unknown <applix.dev@gmail.com>
2018-06-02 01:26:12 +0100

    First try with free mobile api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,44 @@
-wow = new WOW();
-wow.init();
-
 $(document).ready(function() {
-	 
-	 		/**
-	 		 * check if the input has any value (if we've typed into it)
-	 		 */
-	 		$('.form__input').blur(function() {
+    /**
+     * check if the input has any value (if we've typed into it)
+     */
+    $('.form__input').blur(function() {
+        if ($(this).val()) {
+            $(this).closest('.form__wrapper').addClass('form--filled');
+        } else {
+            $(this).closest('.form__wrapper').removeClass('form--filled');
+        }
+    });
 
-				if ($(this).val()) {
-					$(this).closest( '.form__wrapper' ).addClass('form--filled');
-				} else {
-					$(this).closest( '.form__wrapper' ).removeClass('form--filled');
-				}	
-			});
+    $('.form').validate({
+        rules: {
+            identifier: {
+		required: true,
+                minlength: 8,
+		maxlength: 8
+            },
+	    pass: {
+		required: true
+            },
+	    msg: {
+		required: true,
+                maxlength: 480
+            }
+        }
+    });
 
-	 		/**
-	 		 * Form validation
-	 		 */
-			$('.form').validate({
-				rules : {
-					password: {
-						minlength: 5
-					}
-				}
-			}); 
-
-			/**
-			 * Form2 validation
-			 */
-			$('.form2').validate();
-
-
-			/**
-			 * Simple Modal
-			 */
-			$('.modal__toggle').on('click', function (e) {
-	  			e.preventDefault();
-				$('.modal').toggleClass('modal--open');
-			});	
+    $('.form').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            url : 'https://smsapi.free-mobile.fr/sendmsg',
+            type: "GET",
+            data: $(this).serialize(),
+            success: function (data) {
+                $(".form").html(data);
+            },
+            error: function (jXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    });
 });
